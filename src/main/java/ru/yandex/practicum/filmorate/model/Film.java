1a28574f2920cfe6aa2235d6fa36ca15f74514c2
Exist <existqq@gmail.com>
2025-07-08 12:08:23 +0300

    Complete HW 12.1

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.Set;
 import lombok.Builder;
 import lombok.Data;
 
@@ -14,22 +15,51 @@ import lombok.Data;
 public class Film {
   private static final LocalDate CHECK_DATE = LocalDate.of(1895, 12, 28);
 
-  Long id;
+  private Long id;
 
   @NotBlank(message = "Название фильма не может быть пустым")
-  String name;
+  private String name;
 
   @Size(max = 200, message = "Описание не может быть длинее 200 символов")
-  String description;
+  private String description;
 
-  LocalDate releaseDate;
+  private LocalDate releaseDate;
 
   @Positive(message = "Длительность фильма не может быть отрицательной")
-  int duration;
+  private int duration;
+
+  private Set<Genre> genres;
+
+  private MPA mpa;
 
   @AssertTrue(message = "Фильм не может быть раньше 28.12.1895")
   @JsonIgnore
   public boolean isValidReleaseDate() {
     return releaseDate == null || !releaseDate.isBefore(CHECK_DATE);
+  }
+
+  public enum Genre {
+    COMEDY,
+    DRAMA,
+    THRILLER,
+    DOCUMENTARY,
+    CARTOON,
+    ACTION;
+  }
+
+  public enum MPA {
+    G,
+    PG,
+    PG13,
+    R,
+    NC17;
+
+    @Override
+    public String toString() {
+      if (name().length() == 4) {
+        return this.name().substring(0, 2) + "-" + this.name().substring(2);
+      }
+      return this.name();
+    }
   }
 }
