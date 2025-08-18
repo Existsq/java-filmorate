11e0da80dcc47ec34a269fdecbc1806d06ad6492
Dudenko Vadim <dudenko.vadim@gmail.com>
2025-08-18 18:25:52 +0500

    FilmReview structure

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/FilmReview.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FilmReview {
+    private long id;
+    @NotBlank(message = "Комментарий не может быть пустым")
+    private String content;
+    private boolean is_positive;
+    private long film_id;
+    private long user_id;
+    private int usefull;
+}
