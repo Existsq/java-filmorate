adff8ca4c92aa2dad9e35b8a8f80ff00ed21ea6b
Tatyana Volodina <volodina.tv@yandex.ru>
2025-08-20 22:16:49 +0300

    fix for checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.filmorate.service;
 import java.util.Set;
 
 public interface RecommendationService {
+
     Set<Long> getLikedFilmIds(Long userId);
+
     Set<Long> findUsersWithSimilarTastes(Long userId);
 }
