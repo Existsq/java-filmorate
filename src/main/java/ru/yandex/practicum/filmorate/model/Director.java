4abfbbc9da02b216703540a84f71401e082612fe
Exist <148970132+Existsq@users.noreply.github.com>
2025-08-25 20:22:56 +0300

    Add name validation for director entity

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -1,3 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-public record Director(long id, String name) {}
+import jakarta.validation.constraints.NotBlank;
+
+public record Director(long id, @NotBlank(message = "Имя режиссера не может быть пустым") String name) {}
