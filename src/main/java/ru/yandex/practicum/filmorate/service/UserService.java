baa9ec774943fa7bd289c9b57d4653acbc24eb09
Exist <existqq@gmail.com>
2025-08-27 10:11:04 +0300

    style: remove unused import

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import java.util.*;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.model.EventType;
 import ru.yandex.practicum.filmorate.model.OperationType;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
